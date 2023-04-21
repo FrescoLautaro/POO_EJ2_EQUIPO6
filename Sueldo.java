ae1a50e2c4fbaf0344475c5394866588f795318b
Dainobot <ignacio.martin.daino@gmail.com>
2023-04-21 09:57:28 -0300

    daino corrije

--- Sueldo.java
+++ Sueldo.java
@@ -12,6 +12,8 @@ public class Sueldo {
     private double obraSocial;
     private double jubilacion;
     private double presentismo;
+    private double suBasico;
+    private double suNeto;
     private String cargo;
     private AsignacionFamiliar asigFamiliar;
 
@@ -19,8 +21,38 @@ public class Sueldo {
         this.obraSocial = obraSocial;
         this.jubilacion = jubilacion;
         this.presentismo = presentismo;
+        this.suBasico = suBasico;
+        this.suNeto = suNeto;
         this.cargo = cargo;
         this.asigFamiliar = asigFamiliar;
+    }
+    public double suBasico()
+    {
+        double sB = 0.0;
+        if (cargo.equals ("Repostero/a")){
+            sB = 110000;
+            if (genero.equals ("Hombre")){
+                sB = 110000 +  10*100/sB;
+            }
+            
+        }
+        if (cargo.equals ("Vendedor/a")){
+            sB = 70000;
+            
+        }
+        if (cargo.equals ("Panadero/a")){
+            sB = 98000;
+            
+        }
+        return sB;
+    }
+    public void emitirDetalle()
+    {
+        System.out.println("OBRA SOCIAL: "+ this.obraSocial + " JUBILACION: "+ this.jubilacion+" PRESENTISMO: "+this.presentismo+" CARGO: "+ this.cargo+" ASIGNACION FAMILIAR: "+ this.asigFamiliar.asigFamiliar());
+    }
+    public void suNeto()
+    {
+        
     }
     
 
