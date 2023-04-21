a472ca711475f3d10917a4e79b79b4671e8be6b6
Dainobot <ignacio.martin.daino@gmail.com>
2023-04-21 09:52:59 -0300

    Daino añadio sus archivos

--- Sueldo.java
+++ Sueldo.java
@@ -2,12 +2,29 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ejercicio_2_poo;
+package javaapplication1;
 
 /**
  *
  * @author ET36
  */
 public class Sueldo {
+    private double obraSocial;
+    private double jubilacion;
+    private double presentismo;
+    private String cargo;
+    private AsignacionFamiliar asigFamiliar;
+
+    public Sueldo(double obraSocial, double jubilacion, double presentismo, String cargo) {
+        this.obraSocial = obraSocial;
+        this.jubilacion = jubilacion;
+        this.presentismo = presentismo;
+        this.cargo = cargo;
+        this.asigFamiliar = asigFamiliar;
+    }
+    
+
+        
+    
     
 }
